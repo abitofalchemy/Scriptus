c6dae0810e16f1cc529bc0cd684051a3edb946b2
Salvador Aguinaga <sal@windblade.(none)>
2013-02-22 05:19:49 -0500

    first commit

---
+++ ah_login_api/include/config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ *  * Database config variables
+ *   */
+define("DB_HOST", "localhost");
+define("DB_USER", "tremcam");
+define("DB_PASSWORD", "trem00xB");
+define("DB_DATABASE", "neurobit");
+?>
