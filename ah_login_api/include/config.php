824616ea40906b49894db1e7fe8d267096e0b2a1
Salvador Aguinaga <saguinag@nd.edu>
2013-02-22 05:43:07 -0500

    nothing to see

--- ah_login_api/include/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- *  * Database config variables
- *   */
-define("DB_HOST", "localhost");
-define("DB_USER", "tremcam");
-define("DB_PASSWORD", "trem00xB");
-define("DB_DATABASE", "neurobit");
-?>
