1191d5d7bc41466f18ae36de9f4ff6fb6628e17c
abitofalchemy <saguinag@nd.edu>
2018-05-01 10:53:19 -0400

    added the dots from dsg1

---
+++ pyths/prnt_sysinfo.py
@@ -0,0 +1,16 @@
+import platform
+# author: http://sebastianraschka.com/Articles/2014_multiprocessing.html
+
+
+def print_sysinfo():
+
+    print('\nPython version  :', platform.python_version())
+    print('compiler        :', platform.python_compiler())
+
+    print('\nsystem     :', platform.system())
+    print('release    :', platform.release())
+    print('machine    :', platform.machine())
+    print('processor  :', platform.processor())
+    print('CPU count  :', mp.cpu_count())
+    print('interpreter:', platform.architecture()[0])
+    print('\n\n')
